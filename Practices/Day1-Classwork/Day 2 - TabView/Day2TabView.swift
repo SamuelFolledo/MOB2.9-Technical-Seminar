26e069de4d31092e7686db319ff37119a8ef8e8d
Samuel Folledo <samuelfolledo@gmail.com>
2021-04-06 19:03:07 -0400

    [add] added detail page on each tabView

--- Practices/Day1-Classwork/Day 2 - TabView/Day2TabView.swift
+++ Practices/Day1-Classwork/Day 2 - TabView/Day2TabView.swift
@@ -7,43 +7,41 @@
 
 import SwiftUI
 
-struct HomeView: View {
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color.blue
-            }
-            .navigationTitle("Home")
-        }
-    }
-}
-
-struct SettingsView: View {
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color.black
-            }
-            .navigationTitle("Settings")
-        }
-    }
-}
-
 struct Day2TabView: View {
     
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab,
+                content: {
             HomeView()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
+                .onAppear(perform: {
+                    selectedTab = 0
+                })
+                .tag(0)
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Profile")
+                }
+                .onAppear(perform: {
+                    selectedTab = 1
+                })
+                .tag(1)
             SettingsView()
                 .tabItem {
                     Image(systemName: "gear")
                     Text("Settings")
                 }
-        }
+                .onAppear(perform: {
+                    selectedTab = 2
+                })
+                .tag(2)
+        })
     }
 }
 
@@ -52,5 +50,85 @@ struct Day2TabView_Previews: PreviewProvider {
         Day2TabView()
             .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
             .previewDisplayName("User Preview iPhone 8")
+    }
+}
+
+//MARK: - Views
+struct HomeView: View {
+    @State var goToDetail = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue.opacity(0.2)
+                Button(action: {
+                    goToDetail.toggle()
+                }, label: {
+                    Text("Go To Details")
+                })
+                NavigationLink(
+                    destination: TabDetailView(text: "Home"),
+                    isActive: $goToDetail,
+                    label: {
+                        EmptyView()
+                    })
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct SettingsView: View {
+    @State var goToDetail = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.black
+                Button(action: {
+                    goToDetail.toggle()
+                }, label: {
+                    Text("Go To Details")
+                })
+                NavigationLink(
+                    destination: TabDetailView(text: "Settings"),
+                    isActive: $goToDetail,
+                    label: {
+                        EmptyView()
+                    })
+            }
+            .navigationTitle("Settings")
+        }
+    }
+}
+
+struct ProfileView: View {
+    @State var goToDetail = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.gray
+                Button(action: {
+                    goToDetail.toggle()
+                }, label: {
+                    Text("Go To Details")
+                })
+                NavigationLink(
+                    destination: TabDetailView(text: "Profile"),
+                    isActive: $goToDetail,
+                    label: {
+                        EmptyView()
+                    })
+            }
+            .navigationTitle("Profile")
+        }
+    }
+}
+
+struct TabDetailView: View {
+    let text: String
+    var body: some View {
+        Text(text)
     }
 }
